8840d09463a88266be5f8953112d677cc5a12492
lovehzj <1922919664@qq.com>
2022-01-14 20:15:28 +0800

    Release docker image binary

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir $COREDNS_HOME/logs
+mkdir -p $COREDNS_HOME/logs
 nohup $COREDNS_HOME/mate/coredns_mate >>$COREDNS_HOME/logs/coredns_mate.stdout.log 2>>$COREDNS_HOME/logs/coredns_mate.stderr.log
 
